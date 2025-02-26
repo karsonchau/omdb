e862ada5a520827b507e01c592ba031bb8cccee2
Karson Chau <karsonchau@Karsons-MacBook-Pro.local>
2025-02-26 10:34:43 -0800

    chore: change java version to 11 for inline method
    Changed Java version to 11 for Mockito whenever inline
    function. Fixed unit tests

--- app/src/test/java/com/example/omdb/ExampleUnitTest.kt
+++ app/src/test/java/com/example/omdb/ExampleUnitTest.kt
@@ -1,53 +1,38 @@
 package com.example.omdb
 
+
 import com.example.omdb.data.MoviesRepository
-import com.example.omdb.di.AppModule
 import com.example.omdb.model.ApiResult
 import com.example.omdb.model.MovieSearchResult
 import com.example.omdb.network.NetworkObserver
 import com.example.omdb.ui.theme.screens.MovieUiState
 import com.example.omdb.ui.theme.screens.MovieViewModel
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.UninstallModules
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert
 import org.junit.Test
 import org.junit.Before
-import org.junit.Rule
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
+import org.mockito.Mockito
 import org.mockito.kotlin.whenever
-import javax.inject.Inject
 
-@HiltAndroidTest
-@UninstallModules(AppModule::class)
+
 class MovieViewModelTest {
-    @get:Rule
-    var hiltRule = HiltAndroidRule(this)
 
-    @Inject
-    lateinit var movieViewModel: MovieViewModel
+    private lateinit var movieViewModel: MovieViewModel
 
-    @Mock
-    lateinit var moviesRepository: MoviesRepository
+    private val fakeMovieRepository = Mockito.mock(MoviesRepository::class.java)
 
-    @Mock
-    lateinit var networkObserver: NetworkObserver
-
+    private val fakeNetworkObserver = Mockito.mock(NetworkObserver::class.java)
 
     @Before
-    fun setup() {
-        MockitoAnnotations.openMocks(this)
-        hiltRule.inject()
-        movieViewModel = MovieViewModel(moviesRepository, networkObserver)
+    fun injectHiltRule() {
+        movieViewModel = MovieViewModel(moviesRepository = fakeMovieRepository, networkObserver = fakeNetworkObserver)
     }
 
     @Test
-    fun testMoviesSearch() {
+    fun searchMovieByTitle_noNetwork_error() {
         // Arrange: Setup mock repository behavior
         runBlocking {
-            whenever(moviesRepository.getMovies(any(), any(), any(), any()))
+            whenever(fakeMovieRepository.getMovies("Batman", null, null, null))
                 .thenReturn(ApiResult.Success(MovieSearchResult(emptyList(), 0, null)))
         }
 
@@ -55,6 +40,7 @@ class MovieViewModelTest {
         movieViewModel.searchMoviesByTitle("Batman")
 
         // Assert
-        assert(movieViewModel.movieUiState.value is MovieUiState.Success)
+        assert(movieViewModel.movieUiState.value is MovieUiState.Error)
+        Assert.assertEquals("No network connection!", (movieViewModel.movieUiState.value as MovieUiState.Error).message)
     }
 }
